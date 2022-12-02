47458227ef3fed66c3f54f13ddd07fb794482861
Noboru Ando <65776256+moshimoshipandasan@users.noreply.github.com>
2022-12-03 00:10:13 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import numpy as np
 import pandas as pd
 
 # GitHUbにアップするときはこの下に書き換える
-model = load_model('keras_Model.h5', compile=False)
-class_names = open('labels.txt', 'r').readlines()
+model = load_model('/keras_Model.h5', compile=False)
+class_names = open('/labels.txt', 'r').readlines()
 # model = load_model('/content/keras_model.h5', compile=False)
 # class_names = open('/content/labels.txt', 'r').readlines()
 
