76a6ab717f9ba9cce7f07571c340941d1c69761f
Noboru Ando <65776256+moshimoshipandasan@users.noreply.github.com>
2022-12-02 23:15:25 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+# 以下を「app.py」に書き込み
+import streamlit as st
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+import pandas as pd
+
+# GitHUbにアップするときはこの下に書き換える
+model = load_model('keras_Model.h5', compile=False)
+# model = load_model('/content/keras_model.h5', compile=False)
+
+st.set_option("deprecation.showfileUploaderEncoding", False)
+
+st.sidebar.title("AI画像認識アプリ")
+st.sidebar.write("Teachable Machineの学習モデルを使って何の画像かを判定します。")
+
+st.sidebar.write("")
+
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+img_source = st.sidebar.radio("画像のソースを選択してください。",
+                              ("画像をアップロード", "カメラで撮影"))
+if img_source == "画像をアップロード":
+    img_file = st.sidebar.file_uploader("画像を選択してください。", type=["png", "jpg"])
+elif img_source == "カメラで撮影":
+    img_file = st.camera_input("カメラで撮影")
+
+if img_file is not None:
+    with st.spinner("推定中..."):
+        image = Image.open(img_file)
+        size = (224, 224)
+        # 画像をセンタリングし指定したsizeに切り出す処理
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+        st.image(image, caption="対象の画像", width=480)
+        st.write("")
+        image_array = np.asarray(image)
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+        # 例外処理
+        try:
+          data[0] = normalized_image_array
+        except Exception as e:
+          st.write(e)
+        else:
+          prediction = model.predict(data)
+
+        # 円グラフの表示
+        pie_labels = ['none', 'rock', 'scissors', 'paper']
+        pie_probs = prediction[0]
+        st.write(pd.DataFrame(pie_probs,pie_labels))
+        st.subheader('円グラフ')
+        fig, ax = plt.subplots()
+        wedgeprops={"width":0.3, "edgecolor":"white"}
+        textprops = {"fontsize":6}
+        ax.pie(pie_probs, labels=pie_labels, counterclock=False, startangle=90,
+               textprops=textprops, autopct="%.2f", wedgeprops=wedgeprops)  # 円グラフ
+        st.pyplot(fig)
+        st.subheader('表')
